e49bd4d6da328505d3ddce74b71a5f67f2b4ccb8
Jon Yoder <jyoder@bcadmin.com>
2022-01-11 11:08:28 -0500

    Remove hashbuffer, which isn't needed

--- pyeznacl/ezhash.py
+++ pyeznacl/ezhash.py
@@ -48,41 +48,6 @@ def sha3_256hash(data: bytes) -> str:
 	return "SHA3-256:" + base64.b85encode(hasher.digest()).decode()
 
 
-def hashbuffer(data: bytes, algorithm: str) -> RetVal:
-	'''Calculates a hash value for the memory buffer passed to it
-
-	Parameters:
-	  * data: an array of bytes
-	  * algorithm: the hash algorithm to be used. This can be one of the following:
-			- BLAKE2B-256 (default)
-			- BLAKE3-256
-			- SHA-256
-			- SHA3-256
-	
-	Returns:
-	  * hash: (CryptoString) the computed hash of the buffer
-	'''
-	if not data:
-		return RetVal(ErrBadValue, 'bad path')
-	
-	hasher = None
-	if algorithm == 'BLAKE2B-256':
-		hasher = hashlib.blake2b(digest_size=32)
-	elif algorithm == 'BLAKE3-256':
-		hasher = blake3.blake3() # pylint: disable=c-extension-no-member
-	elif algorithm == 'SHA-256':
-		hasher = hashlib.sha256()
-	elif algorithm == 'SHA3-256':
-		hasher = hashlib.sha3_256()
-	else:
-		return RetVal('ErrUnsupported', 'unsupported algorithm')
-	
-	hasher.update(data)
-
-	return RetVal().set_value('hash', 
-		CryptoString(f"{algorithm}:{base64.b85encode(hasher.digest()).decode()}"))
-
-
 def hashfile(path: str, algorithm='BLAKE2B-256') -> RetVal:
 	'''Returns a RetVal containing the hash of the passed file in the 'hash' field. The algorithm 
 	used may be specified, but defaults to BLAKE2B-256.'''
