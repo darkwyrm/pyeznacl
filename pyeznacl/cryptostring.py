2c17fc9dbc98e5c1e05d80f6f354c42402a38927
Jon Yoder <jon@yoder.cloud>
2022-01-22 10:04:12 -0500

    Code polishing

--- pyeznacl/cryptostring.py
+++ pyeznacl/cryptostring.py
@@ -5,15 +5,27 @@ def encode85(b: bytes, pad=False) -> str:
 	'''A string-oriented version of the b85encode function in the base64 module'''
 	return base64.b85encode(b, pad).decode()
 
-def decode85(b) -> bytes:
+def decode85(s: str) -> bytes:
 	'''A string-oriented version of the b85decode function in the base64 module'''
-	return base64.b85decode(b)
+	return base64.b85decode(s.encode())
 
 class CryptoString:
 	'''This class encapsulates code for working with strings associated with an algorithm. This 
 	includes hashes and encryption keys.'''
 
 	def __init__(self, string='', data=None):
+		'''Create a CryptoString object, optionally based on existing data.
+
+		Parameters:
+		string: If used by itself, this is expected to be a CryptoString-formatted string. If the 
+		data parameter is used, this must contain only the CryptoString prefix for the data.
+		data: Raw binary data. If used, the string parameter must contain the string prefix, e.g. 
+		'BLAKE3-256'.
+
+		Notes:
+		The two different ways of using the CryptoString constructor affords great flexibility in 
+		exchange for being a little confusing -- it can be used like set() or set_raw().
+		'''
 		self.prefix = ''
 		self.data = ''
 		
@@ -23,8 +35,15 @@ class CryptoString:
 			self.set(string)
 	
 	def set(self, data: str) -> bool:
-		'''Initializes the instance from data passed to it. The string is expected to follow the 
-		format ALGORITHM:DATA, where DATA is assumed to be base85-encoded raw byte data'''
+		'''Initializes the instance from data passed to it.
+		
+		Parameters:
+		data: A string in the format ALGORITHM:DATA, where DATA is assumed to be base85-encoded raw 
+		byte data
+
+		Returns:
+		True if successfully set.
+		'''
 
 		if not data:
 			self.prefix = ''
@@ -38,9 +57,16 @@ class CryptoString:
 		return True
 
 	def set_raw(self, prefix: str, data: bytes) -> str:
-		'''Initializes the instance to some raw data and a prefix. It returns the resulting string 
-		with the encoded data. If an error occurs, such as if the prefix is not formatted 
-		correctly, an empty string is returned.'''
+		'''Initializes the instance to some raw data and a prefix.
+		
+		Parameters:
+		prefix: The CryptoString prefix for the data
+		data: Raw, unencoded binary data, such as the output from nacl.public.PrivateKey.generate().
+
+		Returns:
+		A CryptoString-formatted string representing the original data. This method returns an 
+		empty string if an error has occurred.
+		'''
 
 		if not (prefix and data):
 			return ''
@@ -102,7 +128,7 @@ def is_cryptostring(string: str) -> bool:
 		return False
 
 	try:
-		_ = base64.b85decode(parts[1])
+		base64.b85decode(parts[1])
 	except ValueError:
 		return False
 	
